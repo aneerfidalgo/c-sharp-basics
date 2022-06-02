293946bff1b573453f5ae3f058dddeac320cbaf7
Aneer Fidalgo <aneer@Aneers-MacBook-Air.local>
2022-06-02 17:07:23 -0400

    miguel helped out

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// See https://aka.ms/new-console-template for more information
+
+
+using Accounts; 
+
+
+BankAccount account = new BankAccount();
+
+Console.WriteLine($"This is my account number {account.AccountNumber} and my bal {account.Balance}. Plus my name is {account.Owner}");
+
+BankAccount account2  = new BankAccount("Nerissa", decimal.MinValue);
+
+Console.WriteLine($"This is my account number 2 {account2.AccountNumber} and my bal {account2.Balance}. Plus my name is {account2.Owner}");
+
+SavingAccount savings = new SavingAccount();
+savings.InterestRate = 0.05m;
+savings.Balance = 2000.0m;
+
+savings.AddMonthlyInterest();
+
+Console.WriteLine($"What is the balance {savings.Balance}");
+
+
+
